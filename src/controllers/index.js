7e03f336f8bac89ea3859c7104fcee6a1e8bc928
abedshamia <ab.shamia96@gmail.com>
2022-03-24 22:41:42 +0200

    Add Delete function in backend;

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 const {getLoginPage, indexPage} = require('./pagesController');
 const {registerUser, loginUser, logoutUser} = require('./authController');
 const {getAllPosts, createPost, deletePost, getPost} = require('./postsController');
+const {getUserId} = require('./userController');
 
 module.exports = {
   getLoginPage,
@@ -12,4 +13,5 @@ module.exports = {
   createPost,
   deletePost,
   getPost,
+  getUserId,
 };

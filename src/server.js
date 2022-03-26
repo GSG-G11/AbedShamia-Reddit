e6306cdc885fdf3de414a624a416cc6cc90735dc
abedshamia <ab.shamia96@gmail.com>
2022-03-26 19:25:25 +0300

    remove cross-env

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
-    const app = require('./app');
-    const connection = require('./database/config/connection');
-    
-    const PORT = process.env.PORT || 3000;
-    
-    const start = async () => {
-      try {
-        await connection.connect();
-        console.log('Connected to the database');
-        app.listen(PORT, () => {
-          console.log('App listening on port ' + PORT);
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    
-    start();
-    
+require('express-async-errors');
+const app = require('./app');
+const connection = require('./database/config/connection');
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connection.connect();
+    console.log('Connected to the database');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
